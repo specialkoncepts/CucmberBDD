0bd4d23810a7355c5e546786f2550bbf34c5dc33
specialkoncepts <kbakaush@gmail.com>
2022-10-30 09:32:16 -0400

    first commit

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BrowserUtils;
+
+public class HomePage {
+
+    public HomePage(){
+        PageFactory.initElements(BrowserUtils.getDriver(), this);
+    }
+
+    @FindBy(id = "sample")
+    public WebElement sampleElement;
+
+
+
+}
