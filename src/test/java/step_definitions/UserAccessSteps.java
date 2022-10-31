34d887d4e802f34519a0db645baa1b1e4ffff277
mycupcake7 <mycupcake7@gmail.com>
2022-10-31 14:13:50 -0400

    IN-2 update

--- src/test/java/step_definitions/UserAccessSteps.java
+++ src/test/java/step_definitions/UserAccessSteps.java
@@ -2,10 +2,11 @@ package step_definitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import pages.CommonPage;
 import pages.HomePage;
-import utils.BrowserUtils;
+import utils.utils.BrowserUtils;
 
 import java.util.Map;
 
@@ -16,38 +17,39 @@ public class UserAccessSteps implements CommonPage
     public UserAccessSteps(){
         this.page = new HomePage();
     }
-    @Given("User open url {string}")
-    public void user_open_url(String string) {
-        BrowserUtils.getDriver();
-    }
 
-    @Then("User login with username\"test@yahoo.com\"")
-    public void user_login_with_username_test_yahoo_com(Map<String, String> map)
+    @Given("User open url")
+    public void userOpenUrl() {
+    }
+    @When("User login with username\"test@yahoo.com\"")
+    public void user_login_with_username_test_yahoo_com()
     {
-        for(String key: map.keySet()){
-                BrowserUtils.sendKeys(
-                        BrowserUtils.getDriver().findElement(By.name(key)), map.get(key)
-                );
-
-        }
-
+        BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_INPUT_FIELD, "Enter Username"))).sendKeys("test@yahoo.com");
     }
-
     @Then("User login with password\"test123\"")
-    public void user_login_with_password_test123() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_login_with_password_test123()
+    {
+        BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_INPUT_FIELD, "Enter Password"))).sendKeys("test123");
     }
-
-    @Then("Verify user Should see {string} button from the nav bar after signed in")
-    public void verify_user_should_see_button_from_the_nav_bar_after_signed_in(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("User click login button")
+    public void userClickLoginButton()
+    {
+        BrowserUtils.click(
+                BrowserUtils.getDriver().findElement(
+                        By.xpath(String.format(XPATH_TEMPLATE_BUTTON, "Login"))));
     }
-
-    @Then("Verify user should not have access to {string} button")
-    public void verify_user_should_not_have_access_to_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("Verify user Should only see sign out button from the nav bar after signed in")
+    public void verifyUserShouldOnlySeeSignOutButtonFromTheNavBarAfterSignedIn()
+    {
+        String btn = "Sign out";
+        String navBtn = "";
+        if(navBtn.equalsIgnoreCase(String.valueOf(equals(BrowserUtils.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_BUTTON, btn )))))))
+        {
+            System.out.println("Test passed");
+        }
+        else
+        {
+            System.out.println("Test failed");
+        }
     }
 }
